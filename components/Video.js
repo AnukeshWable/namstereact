2c2cfd8210620fa5c74ffcef95e432a620cfceb2
Anukesh Wable <anukeshwable2003@gmail.com>
2024-08-25 09:06:13 +0530

    added nish

---
+++ components/Video.js
@@ -0,0 +1,14 @@
+const Video = () => {
+    return (
+        <iframe
+            width="220"
+            height="240"
+            src="https://www.youtube.com/embed/pXfDW7jA0b8"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+        ></iframe>
+    );
+}
+
+export default Video;
