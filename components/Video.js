fb5cd83f94d10a583a73a434951635da36f9dd9b
Anukesh Wable <anukeshwable2003@gmail.com>
2024-08-26 15:52:18 +0530

    jus

--- components/Video.js
+++ components/Video.js
@@ -7,7 +7,7 @@ const Video = (props) => {
             height="240"
             name={props.name1}
             src="https://www.youtube.com/embed/pXfDW7jA0b8"
-            frameBorder="0"
+            
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
         ></iframe>
